f3e24ad6a4b252c7c33822d8e29a1c6c8595ec7c
jhrush <jhrush53@gmail.com>
2021-08-07 02:29:58 -0500

    Quick Commit

---
+++ installForChrome.sh
@@ -0,0 +1,3 @@
+sudo mkdir /usr/lib/chromium/hp
+sudo cp hp.* /usr/lib/chromium/hp
+sudo cp manifest.json /usr/lib/chromium/hp 
---
+++ installForFirefox.sh
@@ -0,0 +1,2 @@
+sudo cp autoconfig.cfg /usr/lib/firefox/
+sudo cp autoconfig.js /usr/lib/firefox/defaults/pref/
