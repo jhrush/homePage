8bfb830e18c664ba56d74c73dcf5904b20cfd60d
jhrush <jhrush53@gmail.com>
2021-08-07 03:22:06 -0500

    Quick Commit

--- installForFirefox.sh
+++ installForFirefox.sh
@@ -1,2 +1,5 @@
 sudo cp autoconfig.cfg /usr/lib/firefox/
 sudo cp autoconfig.js /usr/lib/firefox/defaults/pref/
+sudo mkdir /usr/lib/firefox/hp
+sudo cp hp.* /usr/lib/firefox/hp
+sudo cp manifest.json /usr/lib/firefox/hp
