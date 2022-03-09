984798f52d19553725ac5cab9afd25cb7ecf8a5f
jhrush21 <jhrush53@gmail.com>
2022-03-09 00:30:45 -0600

    Quick Commit

--- hp.js
+++ hp.js
@@ -10,6 +10,12 @@ function startTime() {
   s = checkTime(s);
   let hms = `${h}:${m}:${s}`;
   let utchms = `${today.getUTCHours()}:${m}:${s}`;
+ 
+  if (h < 10) {
+    hms=`0${hms}`;
+    utchms=`0${utchms}`;
+  }
+
   document.getElementById('epoc').innerHTML = "UTC: " + utchms + "~" + epoc;
   document.getElementById('clock').innerHTML =  hms;
   setTimeout(startTime, 1000);
